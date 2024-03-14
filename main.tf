04e699139f976beb3f7313ffab9be15bff1c3f39
lunge <tomamargishvili@gmail.com>
2024-03-14 15:11:08 +0400

    delete auth vars from aws provider

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-    shared_config_files      = ["/home/hgresa/.aws/config"]
-    shared_credentials_files = ["/home/hgresa/.aws/credentials"]
-    profile                  = "personal-acc-terraform-practice"
-}
+provider "aws" {}
 
 variable "project" {
     type = string
